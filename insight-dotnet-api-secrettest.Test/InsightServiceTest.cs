3a58a98485a189b0a4ca737dddb381eb14203726
Terraform User <brett.lewerke@insight.com>
2025-09-18 12:01:46 -0700

    Insight GitHub Repository Template

---
+++ insight-dotnet-api-secrettest.Test/InsightServiceTest.cs
@@ -0,0 +1,26 @@
+using insight_dotnet_api_secrettest.Services;
+using Moq;
+
+
+namespace insight_dotnet_api_secrettest.Test;
+
+[TestClass]
+public class InsightServiceTest
+{
+
+    private Mock<ILogger<InsightService>>? _mockLogger;
+    private InsightService? _service;
+
+    [TestInitialize]
+    public void InitializeTests() {
+        _mockLogger = new Mock<ILogger<InsightService>>();
+        _service = new InsightService(_mockLogger.Object);
+    }
+
+    [TestMethod]
+    public void GivenNoInputs_WhenInsightServiceGetDescriptions_ThenReturns5Strings()
+    {
+        var result = _service?.GetDescriptions();
+        Assert.AreEqual(5, result?.Count(), "GetDescriptions() result should have returned 5 values");
+    }
+}
