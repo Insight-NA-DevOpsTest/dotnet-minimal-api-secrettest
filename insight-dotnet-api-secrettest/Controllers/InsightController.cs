c6b0deb4302fddaacfcee69288c9fba1c57d8ff6
Terraform User <brett.lewerke@insight.com>
2025-09-18 12:00:44 -0700

    Insight GitHub Repository Template

---
+++ insight-dotnet-api-secrettest/Controllers/InsightController.cs
@@ -0,0 +1,25 @@
+using insight_dotnet_api_secrettest.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace insight_dotnet_api_secrettest.Controllers;
+
+[Authorize]
+[ApiController]
+[Route("[controller]")]
+public class InsightController : ControllerBase
+{
+    private readonly IInsightService _service;
+
+    public InsightController(ILogger<InsightController> logger, IInsightService service)
+    {
+        _service = service;
+    }
+
+    [HttpGet]
+    public ActionResult<List<string>> Get()
+    {
+        IEnumerable<string> returnValue = _service.GetDescriptions();
+        return new OkObjectResult(returnValue);
+    }
+}
